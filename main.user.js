729501544f4967212b1db25acc0e0eff32451867
xaby <xabybrad@gmail.com>
2024-03-05 22:01:09 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,6 +1,8 @@
 // ==UserScript==
 // @name     _Twitch, ocultar enlace Discover Luna
 // @author       xaby_xd
+// @description  desc
+// @source      https://github.com/xaby-xd/hide-discover-Luna-twitch
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
 // @version      2024-03-05
 // @include  https://www.twitch.tv/*
