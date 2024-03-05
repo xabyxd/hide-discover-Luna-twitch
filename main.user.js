129a9135a42dbb2ce2f8759af2a5c147dbde7191
xaby <xabybrad@gmail.com>
2024-03-05 21:57:46 +0100

    Create main.user.js

---
+++ main.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name     _Twitch, ocultar enlace Discover Luna
+// @author       xaby_xd
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
+// @version      2024-03-05
+// @include  https://www.twitch.tv/*
+// @grant    GM_addStyle
+// ==/UserScript==
+
+window.addEventListener("load", function () {
+    waitForKeyElements(".top-nav__external-link[data-test-selector='top-nav__presto-link'][href='https://www.amazon.com/luna']", hideElement);
+}, false);
+
+function hideElement(jNode) {
+    jNode.hide();
+}
