58f58585f4a5aecca5cc7910bef7bf3e9bc6f95a
xaby <xabybrad@gmail.com>
2024-03-05 22:16:03 +0100

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -1,18 +1,15 @@
 // ==UserScript==
-// @name     _Twitch, ocultar enlace Discover Luna
+// @name     _Hide Discover Luna Link on Twitch
 // @author       xaby_xd
-// @description  desc
+// @description  Basic script to hide Luna
+// @grant    GM_addStyle
 // @source      https://github.com/xaby-xd/hide-discover-Luna-twitch
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=twitch.tv
 // @version      2024-03-05
 // @include  https://www.twitch.tv/*
-// @grant    GM_addStyle
 // ==/UserScript==
 
-window.addEventListener("load", function () {
-    waitForKeyElements(".top-nav__external-link[data-test-selector='top-nav__presto-link'][href='https://www.amazon.com/luna']", hideElement);
-}, false);
-
-function hideElement(jNode) {
-    jNode.hide();
-}
+(function() {
+    'use strict';
+    GM_addStyle('a.top-nav__external-link[data-test-selector="top-nav__presto-link"][href="https://www.amazon.com/luna"] { display: none !important; }');
+})();
